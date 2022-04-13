5fffeec34f03d3e7e13b60980be8a936a4255017
MarvinGC <marlexgscl@gmail.com>
2022-04-12 22:43:34 -0500

    login from enviroments

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  loginForm: FormGroup;
+  constructor(public formBuilder: FormBuilder) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      correo: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ['', [Validators.required]]
+    });
+  }
 
+  submitForm(){
+    console.log(this.loginForm.valid);
+    console.log(this.loginForm.value);
+    console.log(environment.apiUrl);
+  }
 }
